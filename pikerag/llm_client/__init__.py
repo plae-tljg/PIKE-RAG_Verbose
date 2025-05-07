8dd728692df6d368152cc46d518eb540684bc22b
ChenYang <42430618+chenyang-shanghai@users.noreply.github.com>
2025-05-07 09:37:25 +0800

    Add support to standard OpenAI API (#28)
    Co-authored-by: gw00229025 <gw00229025@tech.gwm.cn>

--- pikerag/llm_client/__init__.py
+++ pikerag/llm_client/__init__.py
@@ -5,6 +5,7 @@ from pikerag.llm_client.azure_meta_llama_client import AzureMetaLlamaClient
 from pikerag.llm_client.azure_open_ai_client import AzureOpenAIClient
 from pikerag.llm_client.base import BaseLLMClient
 from pikerag.llm_client.hf_meta_llama_client import HFMetaLlamaClient
+from pikerag.llm_client.standard_openai_api import StandardOpenAIClient
 
 
-__all__ = ["AzureMetaLlamaClient", "AzureOpenAIClient", "BaseLLMClient", "HFMetaLlamaClient"]
+__all__ = ["AzureMetaLlamaClient", "AzureOpenAIClient", "BaseLLMClient", "HFMetaLlamaClient", "StandardOpenAIClient"]
